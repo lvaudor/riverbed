5ffabe32a41cb722abe5235b29e838140d2e1380
lvaudor <lise.vaudor@ens-lyon.fr>
2020-09-17 14:28:45 +0200

    change doc

--- R/project_on_AB.R
+++ R/project_on_AB.R
@@ -1,17 +1,17 @@
 #' A function to project data on a line defined by two points A & B
 #'
-#' @param x x coordinate of points
-#' @param y y coordinate of points
+#' @param dat tibble with coordinates columns x and y
+#' @param datAB tibble with two rows (points A and B) and coordinates columns x and y
 #' @return xp x coordinate of points projected on the transect
 #' @return yp y coordinate of points projected on the transect
 #' @return xt x coordinate along the transect (new coordinate x)
 #' @return yt y coordinate corresponding to the distance to the transect (new coordinate y)
 #' @export
 #' @examples
-#' dat=tibble(x=c(3.3,5.2,1.6,6.3),
-#'            y=c(2.1,6.3,2.1,1.6))
-#' datAB=tibble(x=c(0.2,4.2),
-#'              y=c(2.6,6.6))
+#' dat=tibble(x=c(866142.3, 866149.9, 866143.7),
+#'            y=c(84496.97, 84496.93, 84498.50))
+#' datAB=tibble(x=c(866141.3,866148.7),
+#'              y=c(84498.97, 84496.50))
 #' dat=project_on_AB(dat,datAB)
 #' ggplot(dat,  aes(x=x,y=y))+
 #'   geom_point(col="red")+
@@ -20,7 +20,7 @@
 #'   geom_point(data=dat,col="blue", aes(x=xp,y=yp))+
 #'   coord_fixed(ratio=1)
 
-project_on_AB=function(dat,datAB,datOg=datAB[1,]){
+ project_on_AB=function(dat,datAB,datOg=datAB[1,]){
   slopeACp=datAB %>%
     summarise(diffx=diff(x),
               diffy=diff(y)) %>%
@@ -41,7 +41,7 @@ project_on_AB=function(dat,datAB,datOg=datAB[1,]){
   if(abs(slopeACp)!=Inf & slopeACp!=0){
     slopeCCp=-1/slopeACp
     interceptACp=datAB %>%
-      filter(point=="a") %>%
+      slice(1) %>%
       mutate(interceptACp=y-slopeACp*x) %>%
       pull(interceptACp)
     interceptCCp=dat %>%
@@ -53,6 +53,8 @@ project_on_AB=function(dat,datAB,datOg=datAB[1,]){
              yt=sqrt((y-yp)^2+(x-xp)^2)) %>%
       select(-interceptCCp)
   }
+  dat=dat %>%
+    mutate(xt=xt-min(xt))
   return(dat)
 }
 
